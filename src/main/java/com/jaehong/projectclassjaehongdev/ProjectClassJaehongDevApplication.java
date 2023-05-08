7542f1909d2f60a5d51f0b75d2eaafa12d409a68
JaehongDev <println@kakao.com>
2023-05-08 22:45:40 +0900

    feat: initali spring boot proejct

---
+++ src/main/java/com/jaehong/projectclassjaehongdev/ProjectClassJaehongDevApplication.java
@@ -0,0 +1,13 @@
+package com.jaehong.projectclassjaehongdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClassJaehongDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectClassJaehongDevApplication.class, args);
+    }
+
+}
