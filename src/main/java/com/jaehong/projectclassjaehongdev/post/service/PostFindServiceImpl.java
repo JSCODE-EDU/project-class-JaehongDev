d3a20aae3cc1df92cfc56266e29f9b8d6132c70d
JaehongDev <println@kakao.com>
2023-05-09 17:38:46 +0900

    feat: 게시글 단건 조회 기능 구현

---
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/service/PostFindServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jaehong.projectclassjaehongdev.post.service;
+
+import com.jaehong.projectclassjaehongdev.config.domain.DomainExceptionCode;
+import com.jaehong.projectclassjaehongdev.post.payload.response.PostFindResponse;
+import com.jaehong.projectclassjaehongdev.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostFindServiceImpl implements PostFindService {
+    private final PostRepository postRepository;
+
+    @Override
+    public PostFindResponse execute(Long postId) {
+        // 조회 결과
+        var postViewed = postRepository.findById(postId)
+                .orElseThrow(() -> DomainExceptionCode.POST_DID_NOT_EXISTS.generateError(postId));
+
+        return PostFindResponse.builder().id(postViewed.getId())
+                .title(postViewed.getTitle())
+                .content(postViewed.getContent())
+                .build();
+    }
+}
