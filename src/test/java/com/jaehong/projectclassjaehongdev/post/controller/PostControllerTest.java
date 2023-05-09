e2634c0693420585dc30a21edbe22d7fe0519fc6
JaehongDev <println@kakao.com>
2023-05-09 14:08:00 +0900

    test: 게시글 삭제 api 테스트 작성

--- src/test/java/com/jaehong/projectclassjaehongdev/post/controller/PostControllerTest.java
+++ src/test/java/com/jaehong/projectclassjaehongdev/post/controller/PostControllerTest.java
@@ -1,14 +1,23 @@
 package com.jaehong.projectclassjaehongdev.post.controller;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jaehong.projectclassjaehongdev.config.domain.DomainExceptionCode;
 import com.jaehong.projectclassjaehongdev.post.payload.request.PostCreateRequest;
+import com.jaehong.projectclassjaehongdev.post.payload.request.PostEditRequest;
 import com.jaehong.projectclassjaehongdev.post.payload.response.PostCreateResponse;
+import com.jaehong.projectclassjaehongdev.post.payload.response.PostEditResponse;
 import com.jaehong.projectclassjaehongdev.post.service.PostCreateService;
+import com.jaehong.projectclassjaehongdev.post.service.PostDeleteService;
+import com.jaehong.projectclassjaehongdev.post.service.PostEditService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -19,21 +28,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DisplayName("post controller 단위 테스트")
 @WebMvcTest(PostController.class)
 class PostControllerTest {
-
-
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper mapper;
     @MockBean
-    private PostCreateService postCreateService;
+    PostEditService postEditService;
+    @MockBean
+    PostCreateService postCreateService;
+    @MockBean
+    PostDeleteService postDeleteService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper mapper;
+
 
     @Nested
     @DisplayName("게시글 생성 api 단위 테스트")
@@ -55,9 +66,9 @@ class PostControllerTest {
                             .accept(MediaType.APPLICATION_JSON)
                             .content(mapper.writeValueAsBytes(request)))
                     .andDo(print())
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(response.getTitle()))
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(response.getContent()));
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.title").value(response.getTitle()))
+                    .andExpect(jsonPath("$.content").value(response.getContent()));
         }
 
         @Test
@@ -75,15 +86,79 @@ class PostControllerTest {
                             .accept(MediaType.APPLICATION_JSON)
                             .content(mapper.writeValueAsBytes(request)))
                     .andDo(print())
-                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(domainException.getCode()));
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(domainException.getCode()))
+                    .andExpect(jsonPath("$.message").value(domainException.getMessage()));
         }
     }
 
     @Nested
     @DisplayName("게시글 수정 api 단위 테스트")
     class PostEditActionTest {
-        
+        @Test
+        void 수정이_정상적으로_처리됩니다() throws Exception {
+            var request = PostEditRequest.builder()
+                    .title("title")
+                    .content("content")
+                    .build();
+            var response = PostEditResponse.builder()
+                    .id(1L)
+                    .title(request.getTitle())
+                    .content(request.getContent())
+                    .build();
+            given(postEditService.execute(1L, request)).willReturn(response);
 
+            mockMvc.perform(patch("/api/post/{postId}", 1L)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id").value(response.getId()))
+                    .andExpect(jsonPath("$.title").value(response.getTitle()))
+                    .andExpect(jsonPath("$.content").value(response.getContent()));
+        }
+
+        @Test
+        void 존재하지_않는_게시글을_수정할_수_없습니다() throws Exception {
+            var request = PostEditRequest.builder()
+                    .title("title")
+                    .content("content")
+                    .build();
+            var domainException = DomainExceptionCode.POST_DID_NOT_EXISTS;
+            given(postEditService.execute(1L, request)).willThrow(domainException.generateError(1L));
+
+            mockMvc.perform(patch("/api/post/{postId}", 1L)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(domainException.getCode()))
+                    .andExpect(jsonPath("$.message").value(String.format(domainException.getMessage(), 1L)));
+        }
+    }
+
+    @Nested
+    @DisplayName("게시글 삭제 api 단위 테스트")
+    class PostDeleteActionTest {
+        @Test
+        void 게시글을_삭제합니다() throws Exception {
+            mockMvc.perform(delete("/api/post/{postId}", 1L))
+                    .andDo(print())
+                    .andExpect(status().isCreated());
+        }
+
+        @Test
+        void 존재하지_않는_게시글은_에러_응답이_발생합니다() throws Exception {
+            var domainException = DomainExceptionCode.POST_DID_NOT_EXISTS;
+            doThrow(domainException.generateError(1L)).when(postDeleteService).execute(1L);
+
+            mockMvc.perform(delete("/api/post/{postId}", 1L))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value(domainException.getCode()))
+                    .andExpect(jsonPath("$.message").value(String.format(domainException.getMessage(), 1L)));
+        }
     }
 }
