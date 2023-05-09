b0b008c0351532c06dd3ff02dae189ed33ab4ce5
JaehongDev <println@kakao.com>
2023-05-09 16:48:27 +0900

    style: api 엔드포인트 /api/post -> /api/posts로 변경

--- src/main/java/com/jaehong/projectclassjaehongdev/post/controller/PostController.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/controller/PostController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 
