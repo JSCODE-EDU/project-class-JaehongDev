9bc8a608291150e8409b861601ddf9b6269776dc
JaehongDev <println@kakao.com>
2023-05-09 11:52:23 +0900

    feat: post 수정 기능 추가

--- src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
@@ -1,14 +1,16 @@
 package com.jaehong.projectclassjaehongdev.post.domain;
 
 
-import com.jaehong.projectclassjaehongdev.post.domain.policy.PostPolicy;
+import com.jaehong.projectclassjaehongdev.post.domain.policy.PostValidationPolicy;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class Post {
-    private final String title;
-    private final String content;
     private Long id;
+    private String title;
+    private String content;
 
     private Post(String title, String content) {
         this.title = title;
@@ -18,8 +20,13 @@ public class Post {
     // 첫 번째 고민 정적 팩토리 메소드로 객체를 생성할때 정해진 규칙을 사용하는 것과 명확한 의미 전달중 어느 것을 선택해야 할까?
     public static Post createNewPost(String title, String content) {
         var post = new Post(title, content);
-        PostPolicy.validatePostCreate(post);
+        PostValidationPolicy.validateAll(post);
         return post;
     }
 
+    public Post updatePost(String editTitle, String editContent) {
+        var updatePost = new Post(this.id, editTitle, editContent);
+        PostValidationPolicy.validateAll(updatePost);
+        return updatePost;
+    }
 }
