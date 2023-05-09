80c6185f63b1174945735bdb28909b1bdb1219fd
JaehongDev <println@kakao.com>
2023-05-09 13:47:46 +0900

    feat: 게시글 수정 api 코드 작성

--- src/main/java/com/jaehong/projectclassjaehongdev/post/controller/PostController.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.jaehong.projectclassjaehongdev.post.controller;
 
 import com.jaehong.projectclassjaehongdev.post.payload.request.PostCreateRequest;
+import com.jaehong.projectclassjaehongdev.post.payload.request.PostEditRequest;
 import com.jaehong.projectclassjaehongdev.post.payload.response.PostCreateResponse;
+import com.jaehong.projectclassjaehongdev.post.payload.response.PostEditResponse;
 import com.jaehong.projectclassjaehongdev.post.service.PostCreateService;
+import com.jaehong.projectclassjaehongdev.post.service.PostEditService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostCreateService postCreateService;
+    private final PostEditService postEditService;
 
     @PostMapping()
     public ResponseEntity<PostCreateResponse> createNewPost(@RequestBody PostCreateRequest postCreateRequest) {
         return ResponseEntity.ok(postCreateService.execute(postCreateRequest));
     }
+
+    @PatchMapping("/{postId}")
+    public ResponseEntity<PostEditResponse> updatePost(@RequestBody PostEditRequest postEditRequest, @PathVariable Long postId) {
+        return ResponseEntity.ok(postEditService.execute(postId, postEditRequest));
+    }
+
+
 }
---
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/payload/response/PostEditResponse.java
@@ -0,0 +1,16 @@
+package com.jaehong.projectclassjaehongdev.post.payload.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+@Getter
+public class PostEditResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+}
