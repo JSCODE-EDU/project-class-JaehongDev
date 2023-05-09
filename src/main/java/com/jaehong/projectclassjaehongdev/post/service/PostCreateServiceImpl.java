4b4a53cebcda76e381dbb7f135ad0deb806acd6a
JaehongDev <println@kakao.com>
2023-05-09 17:38:26 +0900

    feat: 게시글 생성 기능 구현

---
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/service/PostCreateServiceImpl.java
@@ -0,0 +1,26 @@
+package com.jaehong.projectclassjaehongdev.post.service;
+
+import com.jaehong.projectclassjaehongdev.post.domain.Post;
+import com.jaehong.projectclassjaehongdev.post.payload.request.PostCreateRequest;
+import com.jaehong.projectclassjaehongdev.post.payload.response.PostCreateResponse;
+import com.jaehong.projectclassjaehongdev.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostCreateServiceImpl implements PostCreateService {
+    private final PostRepository postRepository;
+
+    @Override
+    public PostCreateResponse execute(PostCreateRequest request) {
+        var newPost = Post.createNewPost(request.getTitle(), request.getContent());
+        var postEntity = postRepository.save(newPost);
+
+        return PostCreateResponse.builder()
+                .id(postEntity.getId())
+                .title(postEntity.getTitle())
+                .content(postEntity.getContent())
+                .build();
+    }
+}
