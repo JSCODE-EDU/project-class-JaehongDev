0b038ea5f1f5c25ef056442d0833e19b94bd9102
JaehongDev <println@kakao.com>
2023-05-11 02:07:49 +0900

    refactor: 생성자를 통한 Post 생성코드 제거

--- src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
@@ -9,14 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Post {
 
@@ -32,27 +30,28 @@ public class Post {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    private Post(Long id, String title, String content) {
+    private Post(Long id, String title, String content, LocalDateTime createdAt) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.createdAt = createdAt;
+        PostValidationPolicy.validateAll(this);
+    }
+
+    private Post(Long id, String title, String content) {
+        this(id, title, content, null);
     }
 
     private Post(String title, String content) {
-        this.title = title;
-        this.content = content;
+        this(null, title, content);
     }
 
     // 고민 정적 팩토리 메소드로 객체를 생성할때 정해진 규칙을 사용하는 것과 명확한 의미 전달중 어느 것을 선택해야 할까?
     public static Post createNewPost(String title, String content) {
-        var post = new Post(title, content);
-        PostValidationPolicy.validateAll(post);
-        return post;
+        return new Post(title, content);
     }
 
     public Post updatePost(String editTitle, String editContent) {
-        var updatePost = new Post(this.id, editTitle, editContent, createdAt);
-        PostValidationPolicy.validateAll(updatePost);
-        return updatePost;
+        return new Post(this.id, editTitle, editContent, createdAt);
     }
 }
