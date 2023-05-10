6394d3dfd690083ba9698d35c212426eb3a5895d
JaehongDev <println@kakao.com>
2023-05-11 02:03:15 +0900

    test: RepositoryTest 중복 코드 제거

--- src/test/java/com/jaehong/projectclassjaehongdev/utils/annotation/RepositoryTest.java
+++ src/test/java/com/jaehong/projectclassjaehongdev/utils/annotation/RepositoryTest.java
@@ -6,13 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Transactional
-@DataJpaTest
 @TestProfile
+@DataJpaTest
+//@Transactional dataJpaTest 에 Transactional 이 선언되어 있음
 public @interface RepositoryTest {
 }
