8c07652c2821aad47ef753e3f0851a354fd3ed20
JaehongDev <println@kakao.com>
2023-05-22 01:26:56 +0900

    feature: DomainControllerAdvice에 AuthenticationException Handler 추가

--- src/main/java/com/jaehong/projectclassjaehongdev/global/controller/DomainControllerAdvice.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/global/controller/DomainControllerAdvice.java
@@ -1,9 +1,11 @@
 package com.jaehong.projectclassjaehongdev.global.controller;
 
+import com.jaehong.projectclassjaehongdev.global.authentication.exception.AuthenticationException;
 import com.jaehong.projectclassjaehongdev.global.domain.DomainException;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,6 +32,16 @@ public class DomainControllerAdvice {
                 .body(ErrorResponse.builder()
                         .code(1)
                         .message("서버에서 알 수 없는 에러가 발생했습니다.")
+                        .build());
+
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<ErrorResponse> throwAuthenticationException(AuthenticationException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ErrorResponse.builder()
+                        .code(2)
+                        .message(exception.getMessage())
                         .build());
 
     }
