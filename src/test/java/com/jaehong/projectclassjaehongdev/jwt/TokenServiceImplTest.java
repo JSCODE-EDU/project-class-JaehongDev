821f8ba2053a13a35c804218c3176f7ae20f02cb
JaehongDev <println@kakao.com>
2023-05-21 23:51:04 +0900

    test: TokenService에서 토큰이 정상적으로 생성, 복호와가 이뤄지는지 검증하는 테스트 작성

--- src/test/java/com/jaehong/projectclassjaehongdev/jwt/TokenServiceImplTest.java
+++ src/test/java/com/jaehong/projectclassjaehongdev/jwt/TokenServiceImplTest.java
@@ -1,20 +1,23 @@
 package com.jaehong.projectclassjaehongdev.jwt;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TokenServiceImplTest {
 
     @Test
-    void 토큰_테스트() {
+    void 토큰이_정상적으로_디코딩_된다() {
         var tokenService = new TokenServiceImpl();
+        var token = tokenService.issuedToken(1L, 60 * 60 * 10000);
+        var id = tokenService.getById(token);
+        Assertions.assertThat(id).isEqualTo(1);
+    }
 
-        var token = tokenService.issuedToken("1", 1);
-
-        System.out.println(token);
-
-        var id = tokenService.getSubject(token);
-        System.out.println(tokenService.verifyToken(token));
-        System.out.println(id);
+    @Test
+    void 기간이_만료된_토큰의_경우_토큰을_사용할_수_없는_토큰으로_표시된다() {
+        var tokenService = new TokenServiceImpl();
+        var token = tokenService.issuedToken(1L, -1); // 현재 시간보다 -1초 늦게 만듦
+        Assertions.assertThat(tokenService.verifyToken(token)).isFalse();
     }
 
 }
