48812498a4d660011c92e70fc77c3e21fc8cc434
JaehongDev <println@kakao.com>
2023-05-27 15:49:37 +0900

    feat: 게시글 entity 추가 및 연관관계 매핑

--- src/main/java/com/jaehong/projectclassjaehongdev/comment/domain/Comment.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/comment/domain/Comment.java
@@ -1,9 +1,38 @@
 package com.jaehong.projectclassjaehongdev.comment.domain;
 
 
+import com.jaehong.projectclassjaehongdev.member.domain.Member;
+import com.jaehong.projectclassjaehongdev.post.domain.Post;
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
+@Table(name = "COMMENTS") // mysql comment와 겹침
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member writer;
+    @ManyToOne
+    @JoinColumn(name = "POSTS_Id")
+    private Post post;
+
 
 }
--- src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
+++ src/main/java/com/jaehong/projectclassjaehongdev/post/domain/Post.java
@@ -1,15 +1,19 @@
 package com.jaehong.projectclassjaehongdev.post.domain;
 
 
+import com.jaehong.projectclassjaehongdev.comment.domain.Comment;
 import com.jaehong.projectclassjaehongdev.member.domain.Member;
 import com.jaehong.projectclassjaehongdev.post.domain.policy.PostValidationPolicy;
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -21,24 +25,28 @@ import org.hibernate.annotations.CreationTimestamp;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     @Column(length = 1000)
     private String content;
 
+    // mapping
+    // 연관관계를 매핑해주는 방법과
+    // 단순하게 id를 가지는 방법중에서 어는 것이 더 좋은 방법일까?
     @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    // mapping
-    // 연관관계를 매핑해주는 방법과
-    // 단순하게 id를 가지는 방법중에서 어는 것이 더 좋은 방법일까?
-
     @ManyToOne
     private Member writer;
+
+    @OneToMany
+    @JoinColumn(name = "POSTS_ID")
+    private List<Comment> comments;
+
 
     private Post(Long id, String title, String content, Member writer, LocalDateTime createdAt) {
         this.id = id;
